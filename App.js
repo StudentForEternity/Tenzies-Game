f15d2471f4664c283872d288dcac739bea62e022
StudentForEternity <109116999+StudentForEternity@users.noreply.github.com>
2023-01-25 19:01:36 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,14 +3,6 @@ import Die from "./Die"
 
 
 export default function App() {
-/**
- * Challenge: Update the array of numbers in state to be
- * an array of objects instead. Each object should look like:
- * { value: <random number>, isHeld: false }
- * 
- * Making this change will break parts of our code, so make
- * sure to update things so we're back to a working state
- */
 
     const [dice, setDice] = React.useState(allNewDice())
     
